e07d60344aaaa6b10c86ec33b09888163c56039a
veera <veeramanicadas.kannan@gmail.com>
2024-11-28 18:41:12 +0100

    test16

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Check if the file exists and read the connection string from it
-if [ ! -f /etc/environment/AIRFLOW__DATABASE__SQL_ALCHEMY_CONN ]; then
-  echo "Error: /etc/environment/AIRFLOW__DATABASE__SQL_ALCHEMY_CONN file does not exist"
-  exit 1
-fi
+# # Check if the file exists and read the connection string from it
+# if [ ! -f /etc/environment/AIRFLOW__DATABASE__SQL_ALCHEMY_CONN ]; then
+#   echo "Error: /etc/environment/AIRFLOW__DATABASE__SQL_ALCHEMY_CONN file does not exist"
+#   exit 1
+# fi
 
-# Read the connection string from the file
-export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$(cat /etc/environment/AIRFLOW__DATABASE__SQL_ALCHEMY_CONN)
+# # Read the connection string from the file
+# export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$(cat /etc/environment/AIRFLOW__DATABASE__SQL_ALCHEMY_CONN)
 
 # Optionally, print the environment variable to verify
 echo "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN is set to: $AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"
